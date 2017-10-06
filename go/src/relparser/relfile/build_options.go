a0e4b88d8c9314f55ca2345c66c2e76849e7a495
Shin Yamamoto <shin@scenee.com>
2017-10-06 13:14:45 +0900

    go/relparser: Check xcodeversion

--- go/src/relparser/relfile/build_options.go
+++ go/src/relparser/relfile/build_options.go
@@ -1,8 +1,12 @@
 package relfile
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"os/exec"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
@@ -16,22 +20,32 @@ type BuildOptions struct {
 
 func (opt *BuildOptions) PrintBuildOptions() {
 
+	version := GetXcodeVersion()
+	verComps := strings.Split(version, ".")
+	major, _ := strconv.Atoi(verComps[0])
+
 	val := reflect.ValueOf(opt).Elem()
 	for i := 0; i < val.NumField(); i++ {
 		valueField := val.Field(i)
 		typeField := val.Type().Field(i)
 		tag := typeField.Tag
 
-		switch valueField.Interface().(type) {
+		optName := strings.Split(tag.Get("yaml"), ",")[0]
+		optValue := valueField.Interface()
+
+		switch optValue.(type) {
 		case string:
 			//fmt.Printf("string: Field Name: %s,\t Field Value: %v,\t Tag Value: %s\n", typeField.Name, valueField.Interface(), tag.Get("yaml"))
-			if valueField.Interface() != "" {
-				fmt.Printf("-%v\n\"%s\"\n", strings.Split(tag.Get("yaml"), ",")[0], valueField.Interface())
+			if optValue != "" {
+				fmt.Printf("-%v\n\"%s\"\n", optName, optValue)
 			}
 		case bool:
 			//fmt.Printf("bool: Field Name: %s,\t Field Value: %v,\t Tag Value: %s\n", typeField.Name, valueField.Interface(), tag.Get("yaml"))
+			if optName == "enableUndefinedBehaviorSanitizer" && major <= 8 {
+				continue
+			}
 			fmt.Printf("-%v\n%v\n",
-				strings.Split(tag.Get("yaml"), ",")[0],
+				optName,
 				func() string {
 					if valueField.Interface().(bool) {
 						return "YES"
@@ -41,4 +55,31 @@ func (opt *BuildOptions) PrintBuildOptions() {
 				}())
 		}
 	}
+}
+
+func GetXcodeVersion() string {
+	var (
+		err    error
+		c      *exec.Cmd
+		buffer bytes.Buffer
+	)
+
+	c = exec.Command("xcodebuild", "-version")
+	c.Stdout = &buffer
+	err = c.Run()
+	if err != nil {
+		return ""
+	}
+
+	scanner := bufio.NewScanner(bytes.NewReader(buffer.Bytes()))
+	var version string
+	for scanner.Scan() {
+		version = strings.Split(scanner.Text(), " ")[1]
+		break
+	}
+	if err := scanner.Err(); err != nil {
+		logger.Fatalf("Failed to read Xcode verion")
+	}
+
+	return version
 }
