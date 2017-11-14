df62d8ca9f0d68edb2f8edfcb112dbba43806a22
Shin Yamamoto <shin@scenee.com>
2017-11-14 14:57:08 +0900

    sample: Link SampleApp and SampleLib

---
+++ sample/SampleApp/ObjCBridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  ObjCBridgingHeader.h
+//  SampleApp
+//
+//  Created by Shin Yamamoto on 2017/11/14.
+//  Copyright © 2017 SCENEE. All rights reserved.
+//
+
+#ifndef ObjCBridgingHeader_h
+#define ObjCBridgingHeader_h
+
+#import "SampleLib.h"
+
+#endif /* ObjCBridgingHeader_h */
--- sample/SampleApp/ViewController.swift
+++ sample/SampleApp/ViewController.swift
@@ -13,13 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        SampleLib.sayHello()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
